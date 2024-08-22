2bc6d11a07d6a7fe99bb4e667c82f75a76a87779
imac27 <frank.siyli@gmail.com>
2024-08-22 08:47:31 +0200

    disable image opti

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     exportTrailingSlash: true,
-    output: 'export'
+    output: 'export',
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
